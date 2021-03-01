aad0aa157a1e9f0532218f382720a354d6e10019
dev <hwonderoftheworld@gmail.com>
2021-03-01 10:41:56 -0500

    small

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -20,7 +20,17 @@ const config: MenuEntry[] = [
         href: '/pool',
       },
     ],
-  }
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'http://www.toastfinance.com/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'http://www.toastfinance.com/pools',
+  },
 ]
 
 export default config
